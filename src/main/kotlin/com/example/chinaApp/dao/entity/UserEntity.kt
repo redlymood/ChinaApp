401cf02542fda25587b7d1c7730c12e14810e341
Mikhail Ivanov <94083405+miv-dev@users.noreply.github.com>
2024-11-17 19:00:48 +0300

    Refactor FeedbackEntity and add db changelog 17-01-changelog.yaml

--- src/main/kotlin/com/example/chinaApp/dao/entity/UserEntity.kt
+++ src/main/kotlin/com/example/chinaApp/dao/entity/UserEntity.kt
@@ -1,49 +1,12 @@
 package com.example.chinaApp.dao.entity
-//
+
 import jakarta.persistence.*
-import lombok.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.OffsetDateTime
 import java.util.*
-//
-//@Entity
-//@Getter
-//@Setter
-//@Table(name = "user")
-//@EntityListeners(
-//    AuditingEntityListener::class
-//)
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-//class UserEntity {
-//    @Id
-//    @Column(name = "id")
-//    @GeneratedValue(strategy = GenerationType.UUID)
-//    private var id: UUID? = null
-//
-//    @Column(name = "email")
-//    private var email: String? = null
-//
-//    @Column(name = "name")
-//    private var name: String? = null
-//
-//    @Column(name = "password")
-//    private var password: String? = null
-//
-//    @Column(name = "role")
-//    private var role: String? = null
-//
-//    @Column(name = "creation_date")
-//    @CreatedDate
-//    private var creationDate: OffsetDateTime? = null
-//
-//    @Column(name = "last_update_date")
-//    @LastModifiedDate
-//    private var lastUpdateDate: OffsetDateTime? = null
-//}
+
 
 
 @Entity
